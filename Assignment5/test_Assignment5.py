2e9da3de4e54d54ea78c5f729dcd74be4d4f7045
KiahFarrington <kiahsydney@gmail.com>
2025-09-26 15:06:36 -0400

    Many test cases complete

--- Assignment5/test_Assignment5.py
+++ Assignment5/test_Assignment5.py
@@ -1,42 +1,46 @@
 import unittest
 from Assignment5 import fahrenheit_to_celsius, fibonacci
 
-#testing fahrenheit test
-def test_freezing_point():
-    assert fahrenheit_to_celsius(32) == 0
+#extensive testing, hopfully getting edge-cases 
+#practicing book's code techniques with methods: each method should have 1 function
+class TestAssignment5(unittest.TestCase):
+    #testing fahrenheit test
+    def test_freezing_point(self):
+        self.assertEqual(fahrenheit_to_celsius(32), 0)
 
-def test_negative_temperature():
-    # -40F should exactly equal -40C
-    assert fahrenheit_to_celsius(-40) == -40
+    def test_negative_temperature(self):
+        # -40F should exactly equal -40C
+        self.assertEqual(fahrenheit_to_celsius(-40), -40)
 
-def test_fahrenheit_float():
-    # f -> c, gets decimal, have it round out to the nearest 2
-    result = fahrenheit_to_celsius(10)
-    assert result == -12.22
-    
-def test_fahrenheit_string():
-    with unittest.raises(TypeError):
-        fahrenheit_to_celsius("freezing")
-
+    def test_fahrenheit_float(self):
+        # f -> c, gets decimal, have it round out to the nearest 2
+        self.assertAlmostEqual(fahrenheit_to_celsius(10), -12.22, places=2)
+        
+    def test_fahrenheit_string(self):
+        with  self.assertRaises(TypeError):
+            fahrenheit_to_celsius("freezing")
 
 
-# testing fibonacci
-def test_fibonacci_zero():
-    assert fibonacci(0) == 0
+    # testing fibonacci
+    def test_fibonacci_zero(self):
+        self.assertEqual(fibonacci(0), 0)
 
-def test_fibonacci_five():
-    assert fibonacci(5) == 5
+    def test_fibonacci_five(self):
+        self.assertEqual(fibonacci(5), 5)
 
-def test_fibonacci_seven():
-    assert fibonacci(7) == 13
-    
-def test_fibonacci_seven():
-    assert fibonacci(10) == 55
+    def test_fibonacci_seven(self):
+        self.assertEqual(fibonacci(7), 13)
+        
+    def test_fibonacci_ten(self):
+        self.assertEqual(fibonacci(10), 55)
 
-def test_fibonacci_float():
-    with unittest.raises(TypeError):
-        fibonacci(3.5)
+    def test_fibonacci_float(self):
+        with self.assertRaises(TypeError):
+            fibonacci(3.5)
 
-def test_fibonacci_negative():
-    with unittest.raises(ValueError):
-        fibonacci(-1)
+    def test_fibonacci_negative(self):
+        with self.assertRaises(ValueError):
+            fibonacci(-1)
+            
+if __name__ == '__main__':
+    unittest.main()
