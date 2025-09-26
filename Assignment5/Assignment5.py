b0b3a6c8db151ca3ba03870037ada178220f4c03
KiahFarrington <kiahsydney@gmail.com>
2025-09-26 16:46:51 -0400

    assignment 5 complete

--- Assignment5/Assignment5.py
+++ Assignment5/Assignment5.py
@@ -1,10 +1,3 @@
-#The first function accepts the temperature in Fahrenheit as an argument 
-# and returns the equivalent temperature in Celsius. Celsius = (Fahrenheit - 32) * 5.0/9.0
-
-#The second function accepts an integer n as an argument and returns the nth Fibonacci number.
-# n = 0 returns 0, n = 1 returns 1. For n > 1, 
-# it should return the sum of the previous 2 Fibonacci numbers
-
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit- 32) * 5.0/9.0
     return round(celsius, 2)
