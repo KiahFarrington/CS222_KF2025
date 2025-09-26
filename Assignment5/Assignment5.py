a00fc9b29fb64d9b9477ea0d8ea38e7d533b3079
KiahFarrington <kiahsydney@gmail.com>
2025-09-26 16:45:54 -0400

    assignment 5 complete

--- Assignment5/Assignment5.py
+++ Assignment5/Assignment5.py
@@ -6,13 +6,39 @@
 # it should return the sum of the previous 2 Fibonacci numbers
 
 def fahrenheit_to_celsius(fahrenheit):
-    # Convert Fahrenheit to Celsius
-    print("Fahrenheit:", fahrenheit)
+    celsius = (fahrenheit- 32) * 5.0/9.0
+    return round(celsius, 2)
     
-    
+      
 def fibonacci(n):
-    print("Fibonacci n:", n)
+    if n < 0:
+        raise ValueError("n must be non-negative")
+    elif n == 0:
+        return 0
+    
+    elif n == 1:
+        return 1
+    
+    a = 0 
+    b = 1
+    for count in range(n - 1):
+        temp = b
+        b = a + b
+        a = temp
+         
+    return b
     
     
-fahrenheit_to_celsius()
-fibonacci()
+def main():
+    #main call for converting temperature
+    tempature = float(input("Input Fahrenheit temperature: ")) 
+    tempature = fahrenheit_to_celsius(tempature)
+    print("In Celsius it is:", tempature , "°C")
+    
+    #main call for  fibonacci
+    num = int(input("Input the nth Fibonacci number you want in the sequence: "))
+    fibResult = fibonacci(num)
+    print("It is:", fibResult)
+
+if __name__ == "__main__": 
+    main()
