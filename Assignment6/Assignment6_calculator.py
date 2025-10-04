5dbf708ea5f0f66a75e8553a09702e8f5be1df61
KiahFarrington <kiahsydney@gmail.com>
2025-10-03 23:12:36 -0400

    finished calculator

--- Assignment6/Assignment6_calculator.py
+++ Assignment6/Assignment6_calculator.py
@@ -1,14 +1,15 @@
 class Calculator:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+    def add(self,x, y):
+        return round((x + y), 2)
 
-    def add(self,x, y): #Returns the sum of x and y
-        return x + y
-
-    def subtract(self, x, y): #Returns the result of subtracting y from x
-        return y - x
-
-calc = Calculator(8,0)
-print(calc.add())
-print(calc.subtract())
+    def subtract(self, x, y): 
+        return round((x - y), 2)
+    
+    def multiply(self, x, y):
+        return round((x * y), 2)
+    
+    def divide(self, x, y):
+        if y == 0:
+            raise ZeroDivisionError ("can't divide by 0")
+        
+        return round( (x / y), 2)
