8c24f1be21c0422d5b4b2ff9249fb2824c688939
KiahFarrington <kiahsydney@gmail.com>
2025-10-03 22:50:34 -0400

    fixed/complete test cases

--- Assignment6/test_assignment6.py
+++ Assignment6/test_assignment6.py
@@ -1,34 +1,41 @@
 import unittest
-from Assignment6.Assignment6_calculator import Calculator
+from Assignment6_calculator import Calculator
 
-class test_Calculator(unittest.TestCase):
+class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
+
     def test_basic_add(self):
         self.assertEqual(self.calc.add(2,16), 18)
-
-    def test_negative_add(self):
-        self.assertEqual(self.calc.add(-2,2), 0) 
     
     def test_decimals_add(self):
         self.assertAlmostEqual(self.calc.add(5.23,1.6),6.83)  
-    
-    def test_correct_type_add(self):
-       with self.assertRaises(TypeError):
-            self.calc.add(5, "hello")
 
 
 
     def test_basic_sub(self):
-        self.assertEqual(self.calc.sub(8,9), 1)
-
-    def test_negative_sub(self):
-        self.assertEqual(self.calc.sub(20,1), -19)
+        self.assertEqual(self.calc.subtract(8,9), 1)
 
     def test_decimals_sub(self):
-        self.assertAlmostEqual(self.calc.sub(4.25,9.45), 5.20)  
+        self.assertAlmostEqual(self.calc.subtract(4.25,9.45), 5.20)  
+
+
+
+    def test_basic_mul(self):
+        self.assertEqual(self.calc.multiply(2,16), 18)
     
-    def test_correct_type_sub(self):
-       with self.assertRaises(TypeError):
-            self.calc.sub(5, "hello")
+    def test_decimals_mul(self):
+        self.assertAlmostEqual(self.calc.multiply(5.23,1.6),6.83)  
+
+
+
+    def test_basic_div(self):
+        self.assertEqual(self.calc.divide(2, 12), 6)
+
+    def test_decimals_div(self):
+        self.assertAlmostEqual(self.calc.divide(3.14, 5.6))
+    
+    def test_div_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(3/0)
